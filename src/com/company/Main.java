eb6c69d8871c9549c199daa5f8d382556f74aa41
Pavel F <pavel.f9@mail.ru>
2021-09-02 23:47:11 +0400

    Hello world test commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       String hello = "Hello";
+       String world = "World";
+       System.out.println(hello + " " + world + "!");
+	// write your code here
+    }
+}
